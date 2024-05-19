b237d2e41f9270e23680177fb910b2654d6d5fe3
Tuna Bozkir <suleymantunabozkir5134@gmail.com>
2024-05-19 16:12:44 +0300

    Update not-found.tsx

--- app/(website)/not-found.tsx
+++ app/(website)/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
       <div className="flex flex-col items-center">
         <p className="text-base font-mono font-semibold text-amber-500">404</p>
         <h1 className="mt-4 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          Page not found or it's under development...
+          Page not found or its under development...
         </h1>
         <p className="mt-4 text-base text-zinc-600 dark:text-zinc-400">
           Sorry, we couldnt find the page youre looking for.
